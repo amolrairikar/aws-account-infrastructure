01d6280e0656ffdd20054b92db49ac91947a2f99
= <arairikar1@gmail.com>
2025-04-21 15:37:01 -0500

    removing timeout variable

--- modules/iam-role/main.tf
+++ modules/iam-role/main.tf
@@ -1,19 +1,5 @@
 resource "aws_iam_role" "this" {
-  name = var.role_name
-
-  # assume_role_policy = jsonencode({
-  #   Version = "2012-10-17"
-  #   Statement = [
-  #     {
-  #       Action = "sts:AssumeRole"
-  #       Effect = "Allow"
-  #       Principal = {
-  #         Service = "${var.service_name}.amazonaws.com"
-  #       }
-  #     },
-  #   ]
-  # })
-
+  name               = var.role_name
   assume_role_policy = var.trust_relationship_policy
 
   tags = {
