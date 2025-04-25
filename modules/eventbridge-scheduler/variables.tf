802b2ef3be2903b4034f0035424d57be74d1231d
= <arairikar1@gmail.com>
2025-04-25 13:07:48 -0500

    Added additional parameters to eventbridge scheduler

--- modules/eventbridge-scheduler/variables.tf
+++ modules/eventbridge-scheduler/variables.tf
@@ -17,3 +17,18 @@ variable "eventbridge_role_arn" {
   description = "The ARN of the role assumed by EventBridge Scheduler to trigger the Lambda function"
   type        = string
 }
+
+variable "schedule_frequency" {
+  description = "How often the scheduler will trigger the target Lambda"
+  type        = string
+}
+
+variable "schedule_timezone" {
+  description = "The timezone the scheduler will use"
+  type        = string
+}
+
+variable "schedule_state" {
+  description = "Either ENABLED or DISABLED, use DISABLED if you want to deploy the scheduler without triggering the Lambda yet"
+  type        = string
+}
